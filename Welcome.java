c9cc51d7aa0779a57d3beb6512ae2cc16c7a5b64
Gitea <gitea@fake.local>
2023-10-27 15:11:41 +0000

    latestVersion

--- Welcome.java
+++ Welcome.java
@@ -4,6 +4,7 @@ public class Welcome{
 
 		System.out.println("### Welcome to CI-CD pipeline ###");
 System.out.println("### Build trigger with Hook ###");
+System.out.println("### This is my latest version ###");
 	}
 
 }
