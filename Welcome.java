787331ed8fabe8e827fc47f4f8f0b9cab46617df
avinashpatelin <avinashpatelin@gmail.com>
2023-10-27 17:09:12 +0000

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -6,6 +6,7 @@ public class Welcome{
 System.out.println("### Build trigger with Hook ###");
 System.out.println("### This is my latest version ###");
 System.out.println("### Trigger manually ###");
+		
 	}
 
 }
